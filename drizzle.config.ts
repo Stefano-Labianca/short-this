59105a917d9797a9a31ddb9a281e0a4a37307f3b
Stefano-Labianca <lstefano@protonmail.com>
2024-03-23 20:33:53 +0100

    removed comments

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -10,13 +10,6 @@ export default {
 	out: './src/lib/server/db/drizzle',
 	driver: 'pg',
 	dbCredentials: {
-		// port: process.env.POSTGRES_PORT
-		// 	? parseInt(process.env.POSTGRES_PORT)
-		// 	: 5432,
-		// host: process.env.POSTGRES_LOCALHOST as string,
-		// user: process.env.POSTGRES_USER as string,
-		// password: process.env.POSTGRES_PASSWORD as string,
-		// database: process.env.POSTGRES_DB as string
 		connectionString: process.env.POSTGRES_URL as string
 	}
 } satisfies Config;
